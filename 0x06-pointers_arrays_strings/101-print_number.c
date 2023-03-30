a406c05ee192ee0a8ee1c7bb6aa91b6d791ae18f
thegr8khalee <kaliibro777@gmail.com>
2023-03-30 06:42:31 -0700

    mm

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,18 +3,18 @@
  * @n: The integer to be printed.
  */
 
-void print_number(int n)
-{
-	unsigned int num = n;
-
+void print_number(int n) {
+	int divisor = 1;
 	if (n < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+		_putchar('_')
+			n = -n
+}
+while (n / divisor >= 10) {
+	divisor *= 10;
+}
+while (divisor != 0) {
+	int digit - n /divisor;
+	_putchar(digit + '0');
+	n %= divisor;
+	divisor /= 10;
 }
