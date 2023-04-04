3fabf1508c963e6a7c445241ac2881aac662b9f8
thegr8khalee <kaliibro777@gmail.com>
2023-04-04 07:20:11 -0700

    mm

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,10 +3,10 @@
  * Write a function that fills memory with a constant byte.
  */
 
-char*_memset(char *s, char b, unsigned int n)
+char*_memset(char *s, char b, unsigned int n);
 {
 	int i = 0;
-	for (; n > 0; i++)
+	for (; n > 0; i++);
 	{
 		s[i] = b;
 		n--;
