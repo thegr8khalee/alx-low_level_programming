d3e18c0c985646d5ef4166f0362a1f134a759791
thegr8khalee <kaliibro777@gmail.com>
2023-04-09 10:03:41 -0700

    mm

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,7 @@
 /** Write a program that prints its name, followed by a new line
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
-{
-	printf("%s\n", argv[0]);
-
-	return (0);
+int main(int argc, char *argv[]) {
+    printf("%s\n", argv[0]);
+    return 0;
 }
