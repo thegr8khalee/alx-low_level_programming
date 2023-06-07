c239ec14b196d060098f51f5d9efc9f0ea1e4ee1
thegr8khalee <kaliibro777@gmail.com>
2023-06-07 12:20:00 -0700

    mm

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* _puts_recursion(char *s)
+ * char *s
+ * return = 0
+ */
+void _puts_recursion(char *s);
+{
+if (*s)
+{
+	_putchar(*s);
+	 _puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+}
