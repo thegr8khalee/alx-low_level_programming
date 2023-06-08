0a5f1548b3e8f30e8bab9fcedf4c3b2ec42d958c
thegr8khalee <kaliibro777@gmail.com>
2023-06-08 06:12:31 -0700

    mm

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,7 +15,8 @@ int wildcmp(char *s1, char *s2)
 		return (1 + wildcmp(s1 + 1, s2 + 1));
 	else if (s1['\0'] == s2['\0'])
 		return (1 + wildcmp(s1, s2));
-
+	else if (*s1 == s2['\0'])
+		return (1 + wildcmp(s1, s2));
 	else
 		return (0);
 }
