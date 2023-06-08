e64aa84eea71568af261352759c0790016896a39
thegr8khalee <kaliibro777@gmail.com>
2023-06-08 05:46:20 -0700

    mm

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings
+ * returns 1 if the strings can be considered identical
+ * otherwise return 0
+ * @s1: first string to compare
+ * @s2: second sring to compare
+ * return: int
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+		return (1);
+	if (s1[0] == s2[0])
+		return (wildcmp(s1 +1, s2+1));
+}
