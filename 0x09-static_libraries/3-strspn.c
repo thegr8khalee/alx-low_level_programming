92d20f9057ff19bccc5ad983f99f5737a91b7b90
thegr8khalee <kaliibro777@gmail.com>
2023-06-09 11:57:17 -0700

    mm

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * Write a function that gets the length of a prefix substring.
+ * Prototype: unsigned int _strspn(char *s, char *accept);
+ * Returns the number of bytes in the initial segment of s which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = o;
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
