b2e467c897bd9eada05ea601794e97450a91dbc4
thegr8khalee <kaliibro777@gmail.com>
2023-06-11 05:58:54 -0700

    mm

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,21 +10,21 @@
  */
 int main(int argc, char* argv[])
 {
+	int cents, coins = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-
-	int coins = 0;
 
 	coins += cents / 25;
 	cents%= 25;
