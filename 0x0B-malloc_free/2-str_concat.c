059839093ce9a48160ac3e83e51d8e73f6ddf87c
thegr8khalee <kaliibro777@gmail.com>
2023-06-13 07:09:48 -0700

    mm

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,7 +37,7 @@ char *str_concat(char *s1, char *s2)
 	for (a = 0; a < len1; a++)
 			conc[a] = s1[a];
 
-	for (b = 0; b > len2; a++, b++)
+	for (b = 0; b < len2; a++, b++)
 			conc[a] = s2[b];
 	conc[len] = '\0';
 
