91385a0defbe85d940f542acb3dac40ea282861e
thegr8khalee <kaliibro777@gmail.com>
2023-06-13 05:39:47 -0700

    mm

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/** 
+ * creste_array - reates an array of chars, and initializes it with a specific char
+ * @size: size of array
+ * @c: char 
+ *
+ * Return: pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char* array;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	       array[i] = c;	
+
+	return(array);
+}
