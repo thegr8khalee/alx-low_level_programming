9e236cd87249e436177fe2964f7671b21a4efbf2
thegr8khalee <kaliibro777@gmail.com>
2023-06-15 09:15:52 -0700

    mm

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,7 +18,7 @@ int *array_range(int min, int max)
 	array = malloc(sizeof(int *) * (max - min + 1));
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < (max - min + 1); i++)
+	for (i = 0; i < (max + 1); i++)
 		array[i] = min++;
 	return (array);
 }
