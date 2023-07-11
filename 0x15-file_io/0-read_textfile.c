774fc369d4d44f8013947a92e8b3beb9ea8ff4cf
thegr8khalee <kaliibro777@gmail.com>
2023-07-11 04:19:56 -0700

    mm

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,42 +9,37 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int filedes;
+	char *buffer;
+	ssize_t bytes;
+	ssize_t bytes_r;
+
 	if (filename == NULL)
 		return (0);
-
-	int filedes = open(filename, O_RDONLY);
-
+	filedes = open(filename, O_RDONLY);
 	if (filedes == -1)
 		return (0);
-
-	char *buffer = malloc(sizeof(char) * (letters + 1));
-
+	buffer = malloc(sizeof(char) * (letters + 1));
 	if (buffer == NULL)
 	{
 		close(filedes);
 		return (0);
 	}
-
-	ssize_t bytes = read(filedes, buffer, letters);
-
+	bytes = read(filedes, buffer, letters);
 	if (bytes == -1)
 	{
 		close(filedes);
 		free(buffer);
 		return (0);
 	}
-
-	ssize_t bytes_r = write(STDOUT_FILENO, buffer, bytes);
-
+	bytes_r = write(STDOUT_FILENO, buffer, bytes);
 	if (bytes_r == -1 || bytes_r != bytes)
 	{
 		close(filedes);
 		free(buffer);
 		return (0);
 	}
-
 	close(filedes);
 	free(buffer);
-
 	return (bytes);
 }
