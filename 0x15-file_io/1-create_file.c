5ad60e0b3651cc30ba1224f528b4fadda28be6e9
thegr8kahlee <kaliibro777@gmail.com>
2023-09-04 11:05:17 -0700

    mm

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int w;
+	int w, len = 0;
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
 	if (filename == NULL)
 		return (-1);
@@ -21,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		w = write(fd, text_content, sizeof(text_content) - 2);
+		w = write(fd, text_content, len);
 		if (w == -1)
 		{
 			close(fd);
