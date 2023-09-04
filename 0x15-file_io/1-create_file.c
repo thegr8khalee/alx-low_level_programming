a1202960f2b4a65f786ec84c705fc00056841977
thegr8kahlee <kaliibro777@gmail.com>
2023-09-04 10:56:37 -0700

    mm

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - ...
+ * @filename: ...
+ * @text_content: ...
+ *
+ * Return: ...
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int w;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		w = write(fd, text_content, sizeof(text_content) - 1);
+		if (w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
