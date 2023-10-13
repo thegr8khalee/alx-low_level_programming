850a0874f96b9ef98ae8c2b3637948a483c65dc2
thegr8khalee <thegr8khalee@gmail.com>
2023-10-13 14:13:33 -0700

    mm

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
-def ispalindrome(n):
-    string = str(n)
-    return string == string[::-1]
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
 
-array = []
-for i in range(999, 99, -1):
-    for j in range(i, 99, -1):
-        if ispalindrome(i * j):
-            array.append(i * j)
-print(max(array))
+largest_palindrome = 0
+
+for i in range(100, 1000):  # Iterate through 3-digit numbers
+    for j in range(i, 1000):  # Start from i to avoid duplicate pairs and only check half of the combinations
+        product = i * j
+        if is_palindrome(product) and product > largest_palindrome:
+            largest_palindrome = product
+
+print("The largest palindrome made from the product of two 3-digit numbers is:", largest_palindrome)
