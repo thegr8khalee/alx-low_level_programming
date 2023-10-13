5e7c3f3244ea641ecfb03f8ecca3572589b8013a
thegr8khalee <thegr8khalee@gmail.com>
2023-10-13 13:29:25 -0700

    mm

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - ...
+ * @h: ...
+ *
+ * Return: ...
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
+}
