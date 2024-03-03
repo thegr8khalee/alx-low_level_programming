331585b7d4718d838fcd30d7528f041583ba98d2
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-03 15:06:42 +0300

    mm

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,15 +5,17 @@
  * @s: ...
  * @b: ...
  * @n: ...
- * Retrun: ...
+ * Return: ...
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
+
 	for (i = 0; n > 0; i++)
 	{
 		s[i] = b;
 		n--;
 	}
+	
 	return (s);
 }
