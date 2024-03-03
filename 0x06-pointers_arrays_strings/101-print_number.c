71c6a5aa1d34fa61429b270d200dde1d625a0416
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-03 14:03:02 +0300

    mm

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,14 +10,19 @@ void print_number(int n)
 	char buffer[20];
 	int i = 0, j = 0;
 	if (n == 0) _putchar('0');
-	if (n < 0){
+	if (n < 0)
+	{
 		_putchar('-');
 		n = -n;
 	}
-	while(n != 0){
+	while(n != 0)
+	{
 		j = n % 10;
 		buffer[i++] = '0' + j;
 		n = n / 10;
 	}
-	while (i > 0) _putchar(buffer[--i]);
+	while (i > 0)
+	{
+		_putchar(buffer[--i]);
+	}
 }
