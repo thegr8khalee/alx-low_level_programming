be3ff6b16dbee0de3c3ac99e0c38409f2a9328b3
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-04 18:46:54 +0300

    mm

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,7 +3,6 @@
 
 /** _strdup - duplicate a string.
  * @str: pointer to the string to duplicate
- *
  * Return: return the duplicate pointer
  */
 char *_strdup(char *str)
@@ -13,21 +12,17 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-
 	while (str[len] != '\0')
 		len++;
-
 	if (str != NULL)
 	{
 		dup = (char *)malloc((sizeof(char) * len) + 1);
 	}
-
 	if (dup == NULL)
 		return (NULL);
 
 	for (i = 0; i < len; i++)
 		dup[i] = str[i];
 	dup[len] = '\0';
-
 	return (dup);
 }
