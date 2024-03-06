4f8c30fd02ee27a1101469a622bfbabc58aafe2a
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-06 21:29:01 +0300

    mm

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,15 +10,16 @@
  */
 int *array_range(int min, int max)
 {
-	int i;
+	int i, size;
 	int *array;
 
+	size = max - min + 1;
 	if (min > max)
 		return (NULL);
-	array = malloc(sizeof(int) * (max - min + 1));
+	array = malloc(sizeof(int) * size);
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i <= ((max - min) + 1); i++)
+	for (i = 0; i <= size; i++)
 		array[i] = min++;
 	return (array);
 }
