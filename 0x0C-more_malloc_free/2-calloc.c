44bef723d172c8a3c9bd170d6ccc0e5626ab1aef
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-06 21:13:44 +0300

    mm

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
  
-	result = malloc((size * nmemb));
+	result = malloc(size * nmemb);
 	if (result == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
