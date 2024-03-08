f77419703ebdc24f0d15f2114125def1edafb657
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-08 21:44:27 +0300

    mm

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,23 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * free_listint2 - ...
- * @head: ...
+ * free_listint2 - function that frees a listint_t list
+ * @head: a double pointer of lists
  *
- * Return: ...
+ * Return: void
  */
+
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *next;
 
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		temp = (*head)->next;
-		free(temp);
-		*head = temp;
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
 }
