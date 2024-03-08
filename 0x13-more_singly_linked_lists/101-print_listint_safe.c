b3c13cf8a41316e06f4ab6d1fb1dfa33b927f87b
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-08 22:01:21 +0300

    mm

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -39,22 +39,23 @@ size_t print_listint_safe(const listint_t *head)
  *
  * Return: ...
  */
-const listint_t **rev(const listint_t **list, size_t size, const listint_t *new)
+const listint_t **rev(const listint_t **list, size_t size,
+						 const listint_t *new)
 {
-        const listint_t **newlist;
-        size_t i;
+		const listint_t **newlist;
+		size_t i;
 
-        newlist = malloc(size * sizeof(listint_t *));
-        if (newlist == NULL)
-        {
-                free(list);
-                exit(98);
-        }
-        for (i = 0; i < size - 1; i++)
+		newlist = malloc(size * sizeof(listint_t *));
+		if (newlist == NULL)
+		{
+				free(list);
+				exit(98);
+		}
+		for (i = 0; i < size - 1; i++)
 		{
 			newlist[i] = list[i];
 		}
-        newlist[i] = new;
-        free(list);
-        return (newlist);
+		newlist[i] = new;
+		free(list);
+		return (newlist);
 }
