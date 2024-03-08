76f1750fa8fdbd87dbabdf40af765fe02b83c804
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2024-03-08 22:10:22 +0300

    mm

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,49 +1,17 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * free_listint_safe - ...
- * @h: ...
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
  *
- * Return: ...
+ * Return: pointer to the new list
  */
-size_t free_listint_safe(listint_t **h)
-{
-	size_t i, x = 0;
-	listint_t **list = NULL;
-	listint_t *next;
-
-	if (h == NULL || *h == NULL)
-		return (x);
-	while (*h != NULL)
-	{
-		for (i = 0; i < x; i++)
-		{
-			if (*h == list[i])
-			{
-				*h = NULL;
-				free(list);
-				return (x);
-			}
-		}
-		x++;
-		list = re_v(list, x, *h);
-		next = (*h)->next;
-		free(*h);
-		*h = next;
-	}
-	free(list);
-	return (x);
-}
-
-/**
- * re_v - ...
- * @list: ...
- * @size: ...
- * @new: ...
- *
- * Return: ...
- */
-listint_t **re_v(listint_t **list, size_t size, listint_t *new)
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
 	listint_t **newlist;
 	size_t i;
@@ -55,7 +23,43 @@ listint_t **re_v(listint_t **list, size_t size, listint_t *new)
 		exit(98);
 	}
 	for (i = 0; i < size - 1; i++)
-		newlist[i] = new;
+		newlist[i] = list[i];
+	newlist[i] = new;
 	free(list);
 	return (newlist);
+}
+
+/**
+ * free_listint_safe - frees a listint_t linked list.
+ * @head: double pointer to the start of the list
+ *
+ * Return: the number of nodes in the list
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	size_t i, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
+
+	if (head == NULL || *head == NULL)
+		return (num);
+	while (*head != NULL)
+	{
+		for (i = 0; i < num; i++)
+		{
+			if (*head == list[i])
+			{
+				*head = NULL;
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (num);
 }
